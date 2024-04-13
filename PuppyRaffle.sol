b3af16a1784fe392383b897f6870253d66c19f78
Yash Sanjay Bonde <103805387+BondeYash@users.noreply.github.com>
2024-04-13 13:26:53 +0530

    Add files via upload

---
+++ PuppyRaffle.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+//@audit-info use of floating pragma
+// @audit-info why are you using the old version of solc 
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {Base64} from "lib/base64/base64.sol";
+
+/// @title PuppyRaffle
+/// @author PuppyLoveDAO
+/// @notice This project is to enter a raffle to win a cute dog NFT. The protocol should do the following:
+/// 1. Call the `enterRaffle` function with the following parameters:
+///    1. `address[] participants`: A list of addresses that enter. You can use this to enter yourself multiple times, or yourself and a group of your friends.
+/// 2. Duplicate addresses are not allowed
+/// 3. Users are allowed to get a refund of their ticket & `value` if they call the `refund` function
+/// 4. Every X seconds, the raffle will be able to draw a winner and be minted a random puppy
+/// 5. The owner of the protocol will set a feeAddress to take a cut of the `value`, and the rest of the funds will be sent to the winner of the puppy.
+contract PuppyRaffle is ERC721, Ownable {
+    using Address for address payable;
+
+    uint256 public immutable entranceFee;
+
+    address[] public players;
+    uint256 public raffleDuration;
+    uint256 public raffleStartTime;
+    address public previousWinner;
+
+    // We do some storage packing to save gas
+    address public feeAddress;
+    uint64 public totalFees = 0;
+
+    // mappings to keep track of token traits
+    mapping(uint256 => uint256) public tokenIdToRarity;
+    mapping(uint256 => string) public rarityToUri;
+    mapping(uint256 => string) public rarityToName;
+
+    // Stats for the common puppy (pug)
+    string private commonImageUri = "ipfs://QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8";
+    uint256 public constant COMMON_RARITY = 70;
+    string private constant COMMON = "common";
+
+    // Stats for the rare puppy (st. bernard)
+    string private rareImageUri = "ipfs://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
+    uint256 public constant RARE_RARITY = 25;
+    string private constant RARE = "rare";
+
+    // Stats for the legendary puppy (shiba inu)
+    string private legendaryImageUri = "ipfs://QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU";
+    uint256 public constant LEGENDARY_RARITY = 5;
+    string private constant LEGENDARY = "legendary";
+
+    // Events
+    event RaffleEnter(address[] newPlayers);
+    event RaffleRefunded(address player);
+    event FeeAddressChanged(address newFeeAddress);
+
+    /// @param _entranceFee the cost in wei to enter the raffle
+    /// @param _feeAddress the address to send the fees to
+    /// @param _raffleDuration the duration in seconds of the raffle
+    constructor(uint256 _entranceFee, address _feeAddress, uint256 _raffleDuration) ERC721("Puppy Raffle", "PR") {
+        // @audit-info check for Zero address
+        // input validation
+        entranceFee = _entranceFee;
+        feeAddress = _feeAddress;
+        raffleDuration = _raffleDuration;
+        raffleStartTime = block.timestamp;
+
+        rarityToUri[COMMON_RARITY] = commonImageUri;
+        rarityToUri[RARE_RARITY] = rareImageUri;
+        rarityToUri[LEGENDARY_RARITY] = legendaryImageUri;
+
+        rarityToName[COMMON_RARITY] = COMMON;
+        rarityToName[RARE_RARITY] = RARE;
+        rarityToName[LEGENDARY_RARITY] = LEGENDARY;
+    }
+
+    /// @notice this is how players enter the raffle
+    /// @notice they have to pay the entrance fee * the number of players
+    /// @notice duplicate entrants are not allowed
+    /// @param newPlayers the list of players to enter the raffle
+    // @audit There is a flaw of Denail of Service attack where multiple user are added in array can lead 
+    // to more usage of gas and can create a flaw in contract 
+   function enterRaffle(address[] memory newPlayers) public payable {
+        require(msg.value == entranceFee * newPlayers.length, "PuppyRaffle: Must send enough to enter raffle");
+        for (uint256 i = 0; i < newPlayers.length; i++) {
+            players.push(newPlayers[i]);
+        }
+
+        // Check for duplicates
+        for (uint256 i = 0; i < players.length - 1; i++) {
+            for (uint256 j = i + 1; j < players.length; j++) {
+                require(players[i] != players[j], "PuppyRaffle: Duplicate player");
+            }
+        }
+        emit RaffleEnter(newPlayers);
+    }
+    /// @param playerIndex the index of the player to refund. You can find it externally by calling `getActivePlayerIndex`
+    /// @dev This function will allow there to be blank spots in the array
+    // @audit There is an reentrancy fault in this function 
+    // @audit you need to update the state of ether first rather than transferring ether 
+    function refund(uint256 playerIndex) public {
+        // @audit reentrancy attack detected 
+
+        address playerAddress = players[playerIndex];
+        require(playerAddress == msg.sender, "PuppyRaffle: Only the player can refund");
+        require(playerAddress != address(0), "PuppyRaffle: Player already refunded, or is not active");
+
+        // @audit reentrancy 
+        payable(msg.sender).sendValue(entranceFee); // sending eth 
+
+        players[playerIndex] = address(0);  // changind state
+        emit RaffleRefunded(playerAddress);
+    }
+
+    /// @notice a way to get the index in the array
+    /// @param player the address of a player in the raffle
+    /// @return the index of the player in the array, if they are not active, it returns 0
+    function getActivePlayerIndex(address player) external view returns (uint256) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i] == player) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /// @notice this function will select a winner and mint a puppy
+    /// @notice there must be at least 4 players, and the duration has occurred
+    /// @notice the previous winner is stored in the previousWinner variable
+    /// @dev we use a hash of on-chain data to generate the random numbers
+    /// @dev we reset the active players array after the winner is selected
+    /// @dev we send 80% of the funds to the winner, the other 20% goes to the feeAddress
+    // @audit access control not provided PuppyRaffle:selectWinner() should only be called by owner
+    // @audit The block.difficulty is not secure for randomness we can use alternative oracle 
+    function selectWinner() external {
+        // q. Is here a CEI check
+        // q. Do raffleStartTime and raffleDuration are set properly
+        // q. Can any one in this protocol call this function to select winner 
+        require(block.timestamp >= raffleStartTime + raffleDuration, "PuppyRaffle: Raffle not over");
+        require(players.length >= 4, "PuppyRaffle: Need at least 4 players");
+    
+        // @audit Weak randomness check 
+        uint256 winnerIndex =
+            uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length;
+        address winner = players[winnerIndex];
+        uint256 totalAmountCollected = players.length * entranceFee;
+        uint256 prizePool = (totalAmountCollected * 80) / 100;
+        uint256 fee = (totalAmountCollected * 20) / 100;
+
+        // @audit Arithmetic Overflow can occur
+        totalFees = totalFees + uint64(fee);
+
+        uint256 tokenId = totalSupply();
+
+        // We use a different RNG calculate from the winnerIndex to determine rarity
+        // @audit randomness weak Weak PRNG
+        uint256 rarity = uint256(keccak256(abi.encodePacked(msg.sender, block.difficulty))) % 100;
+        if (rarity <= COMMON_RARITY) {
+            tokenIdToRarity[tokenId] = COMMON_RARITY;
+        } else if (rarity <= COMMON_RARITY + RARE_RARITY) {
+            tokenIdToRarity[tokenId] = RARE_RARITY;
+        } else {
+            tokenIdToRarity[tokenId] = LEGENDARY_RARITY;
+        }
+
+        delete players; // e resseting the players array
+        raffleStartTime = block.timestamp;  // e resseting the timestamp
+        previousWinner = winner; // selecting the winner
+        // @audit possible reentrancy attack use CEI
+        (bool success,) = winner.call{value: prizePool}("");
+        require(success, "PuppyRaffle: Failed to send prize pool to winner");
+        _safeMint(winner, tokenId);
+    }
+
+    /// @notice this function will withdraw the fees to the feeAddress
+    // @audit Dangerousv sending eth to the arbitrary address 
+    function withdrawFees() external {
+        // @audit misHanding of Eth
+        require(address(this).balance == uint256(totalFees), "PuppyRaffle: There are currently players active!");
+        uint256 feesToWithdraw = totalFees;
+        totalFees = 0;
+        // slither-disable-next-line arbitrary-send-eth
+        (bool success,) = feeAddress.call{value: feesToWithdraw}("");
+        require(success, "PuppyRaffle: Failed to withdraw fees");
+    }
+
+    /// @notice only the owner of the contract can change the feeAddress
+    /// @param newFeeAddress the new address to send fees to
+    function changeFeeAddress(address newFeeAddress) external onlyOwner {
+        feeAddress = newFeeAddress;
+        emit FeeAddressChanged(newFeeAddress);
+    }
+
+    /// @notice this function will return true if the msg.sender is an active player
+    // @audit This function has never been used still taking the space
+    function _isActivePlayer() internal view returns (bool) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i] == msg.sender) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// @notice this could be a constant variable
+    function _baseURI() internal pure returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    /// @notice this function will return the URI for the token
+    /// @param tokenId the Id of the NFT
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "PuppyRaffle: URI query for nonexistent token");
+
+        uint256 rarity = tokenIdToRarity[tokenId];
+        string memory imageURI = rarityToUri[rarity];
+        string memory rareName = rarityToName[rarity];
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(),
+                            '", "description":"An adorable puppy!", ',
+                            '"attributes": [{"trait_type": "rarity", "value": ',
+                            rareName,
+                            '}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
+}
